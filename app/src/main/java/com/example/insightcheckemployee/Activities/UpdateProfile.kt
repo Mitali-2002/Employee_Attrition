822a2353dc8703a99f742e501efa566fb609babb
PrachiPanande <panandeprachi@gmail.com>
2024-04-06 00:00:28 +0530

    prachi commit

---
+++ app/src/main/java/com/example/insightcheckemployee/Activities/UpdateProfile.kt
@@ -0,0 +1,12 @@
+package com.example.insightcheckemployee.Activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.insightcheckemployee.R
+
+class UpdateProfile : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_update_profile)
+    }
+}
